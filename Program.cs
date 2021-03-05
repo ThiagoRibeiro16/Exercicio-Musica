63ce9cbe312588cc399c5b561697693c7cbfb6dd
Thiago Ribeiro <thiago.escola321@gmail.com>
2021-03-05 05:13:52 -0300

    Exercício concluído

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Exercicio_Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.ForegroundColor=ConsoleColor.DarkCyan;
+
+            Console.WriteLine("Recairei");
+            Console.WriteLine();
+            Console.ForegroundColor=ConsoleColor.DarkGreen;
+
+            Console.WriteLine("Já faz uma semana que eu tô limpo de você");
+            Console.WriteLine("Iê iê, iê iê");
+            Console.WriteLine("E de olhar os seus stories, não sinto saudade");
+            Console.WriteLine("Zero curtida, zero vontade de te ver");
+
+            Console.WriteLine();
+            Console.WriteLine("De beijar sua boca");
+            Console.WriteLine("E dormir de conchinha, sem roupa");
+            Console.WriteLine("E fazer um love, love com você");
+            Console.WriteLine();
+
+            Console.ForegroundColor=ConsoleColor.DarkRed;
+            Console.WriteLine("Eu já te superei");
+            Console.WriteLine("Certeza, eu superei");
+            Console.WriteLine("Mas não manda mensagem outra vez");
+            Console.WriteLine("Senão recairei");
+            Console.WriteLine();
+
+            Console.WriteLine("Eu já te superei");
+            Console.WriteLine("Certeza, eu superei");
+            Console.WriteLine("Mas não manda mensagem outra vez");
+            Console.WriteLine("Senão recairei");
+            Console.WriteLine();
+
+            Console.ForegroundColor=ConsoleColor.DarkGreen;
+            Console.WriteLine("Já faz uma semana que eu tô limpo de você");
+            Console.WriteLine("Iê iê, iê iê");
+            Console.WriteLine("E de olhar os seus stories, não sinto saudade");
+            Console.WriteLine("Zero curtida, zero vontade de te ver");
+
+            Console.WriteLine();
+            Console.WriteLine("De beijar sua boca");
+            Console.WriteLine("E dormir de conchinha, sem roupa");
+            Console.WriteLine("E fazer um love, love com você");
+            Console.ForegroundColor=ConsoleColor.DarkRed;
+
+            Console.WriteLine();
+            Console.WriteLine("Eu já te superei");
+            Console.WriteLine("Certeza, eu superei");
+            Console.WriteLine("Mas não manda mensagem outra vez");
+            Console.WriteLine("Senão recairei");
+
+            Console.WriteLine();
+            Console.WriteLine("Eu já te superei");
+            Console.WriteLine("Certeza, eu superei");
+            Console.WriteLine("Mas não manda mensagem outra vez");
+            Console.WriteLine("Senão recairei");
+
+            Console.WriteLine();
+            Console.WriteLine("Eu já te superei");
+            Console.WriteLine("Certeza, eu superei");
+            Console.WriteLine("Mas não manda mensagem outra vez");
+            Console.WriteLine("Senão recairei");
+            Console.ForegroundColor=ConsoleColor.White;
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+        }
+    }
+}
